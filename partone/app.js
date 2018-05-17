722661eb3f25a0fece78440595d9ff1152a0b4a2
Jonathan Campos <jonbcampos@gmail.com>
2018-05-17 09:28:30 -0500

    changed around the default to something usable

--- partone/app.js
+++ partone/app.js
@@ -1,48 +1,88 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+'use strict';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const config = require('./config');
+const healthCheck = require('express-healthcheck');
+const pkg = require('./package');
+const bodyParser = require('body-parser');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+//-------------------------------------
+//
+// setup the app
+//
+//-------------------------------------
+const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+//-------------------------------------
+//
+// we are using json for everything that this service handles
+//
+//-------------------------------------
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.set('Content-Type', 'application/json');
+    next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//-------------------------------------
+//
+// setup some routes for us to hit
+//
+//-------------------------------------
+app.use('/', function (req, res, next) {
+    const remoteAddress = req.connection.remoteAddress;
+    const hostName = os.hostname();
+    res.status(200).json({ remoteAddress, hostName });
+});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(`/${config.get('POD_ENDPOINT')}`, function (req, res, next) {
+    const remoteAddress = req.connection.remoteAddress;
+    const hostName = os.hostname();
+    res.status(200).json({ remoteAddress, hostName });
+});
+
+//-------------------------------------
+//
+// other helpful routes when working with kubernetes
+//
+//-------------------------------------
+app.use('/healthcheck', healthCheck());
+app.use('/readiness', function (req, res, next) {
+    res.status(200).json({ ready: true });
+});
+app.get('/version', function (req, res, next) {
+    const version = pkg.version;
+    res.status(200).json({ version });
+});
+
+//-------------------------------------
+//
+// catch 404 and forward to error handler
+//
+//-------------------------------------
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+//-------------------------------------
+//
+// error handler
+//
+//-------------------------------------
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    err.response = {
+        message: err.message,
+        internalCode: err.code
+    };
+    // render the error page
+    res.status(err.status || 500).json(err.response);
 });
 
 module.exports = app;
