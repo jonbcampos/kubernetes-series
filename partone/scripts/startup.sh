7d29d55e40d97e836794d5071defc4f0b5b929a8
Jonathan Campos <jonbcampos@gmail.com>
2018-05-18 14:03:47 -0500

    adding wait logic

--- partone/scripts/startup.sh
+++ partone/scripts/startup.sh
@@ -41,4 +41,3 @@ gcloud services enable cloudbuild.googleapis.com
 
 echo "building containers"
 gcloud container builds submit -t gcr.io/${GCLOUD_PROJECT}/${CONTAINER_NAME} ../
-#gcloud container builds submit -t gcr.io/wrkerz-test-3/partone-container ../
