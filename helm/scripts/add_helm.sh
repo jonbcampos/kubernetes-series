25ce93a3289e9008f5145d7a650716d765dd75fb
Jonathan Campos <jonbcampos@gmail.com>
2018-08-05 19:55:44 -0500

    tweaks

--- helm/scripts/add_helm.sh
+++ helm/scripts/add_helm.sh
@@ -3,7 +3,9 @@
 echo "install helm"
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 kubectl --namespace kube-system create sa tiller
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+kubectl create clusterrolebinding tiller \
+    --clusterrole cluster-admin \
+    --serviceaccount=kube-system:tiller
 
 echo "initialize helm"
 #helm init \
@@ -13,16 +15,14 @@ echo "initialize helm"
 #    --tiller-tls-verify \
 #    --tls-ca-cert ../keys/ca.cert.pem \
 #    --service-account tiller
-helm init \
-    --service-account tiller
+helm init --service-account tiller
 helm repo update
 
 echo "move keys"
-cp ../keys/ca.cert.pem $(helm home)/ca.pem
-cp ../keys/helm.cert.pem $(helm home)/cert.pem
-cp ../keys/helm.key.pem $(helm home)/key.pem
-#helm ls --tls
+# cp ../keys/ca.cert.pem $(helm home)/ca.pem
+# cp ../keys/helm.cert.pem $(helm home)/cert.pem
+# cp ../keys/helm.key.pem $(helm home)/key.pem
+# helm ls --tls
 
 echo "verify helm"
 kubectl get deploy,svc tiller-deploy -n kube-system
-
--- helm/scripts/startup.sh
+++ helm/scripts/startup.sh
@@ -16,19 +16,27 @@ gcloud services enable compute.googleapis.com
 gcloud services enable container.googleapis.com
 
 echo "creating container engine cluster"
-gcloud container clusters create ${CLUSTER_NAME} --preemptible --zone ${INSTANCE_ZONE} --scopes cloud-platform --num-nodes 3
+gcloud container clusters create ${CLUSTER_NAME} \
+    --preemptible \
+    --zone ${INSTANCE_ZONE} \
+    --scopes cloud-platform \
+    --enable-autorepair \
+    --enable-autoscaling --min-nodes 1 --max-nodes 4 \
+    --num-nodes 3
 
 echo "confirm cluster is running"
 gcloud container clusters list
 
 echo "get credentials"
-gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${INSTANCE_ZONE}
+gcloud container clusters get-credentials ${CLUSTER_NAME} \
+    --zone ${INSTANCE_ZONE}
 
 echo "confirm connection to cluster"
 kubectl cluster-info
 
 echo "create cluster administrator"
-kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account)
+kubectl create clusterrolebinding cluster-admin-binding \
+    --clusterrole=cluster-admin --user=$(gcloud config get-value account)
 
 echo "confirm the pod is running"
 kubectl get pods
