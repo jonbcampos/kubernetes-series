eaf213ae14674a7d67929dc54d5a4faaa31602eb
Jonathan Campos <jonbcampos@gmail.com>
2018-08-05 20:35:27 -0500

    working on secure option

--- helm/scripts/add_helm.sh
+++ helm/scripts/add_helm.sh
@@ -8,21 +8,8 @@ kubectl create clusterrolebinding tiller \
     --serviceaccount=kube-system:tiller
 
 echo "initialize helm"
-#helm init \
-#    --tiller-tls \
-#    --tiller-tls-cert ../keys/tiller.cert.pem \
-#    --tiller-tls-key ../keys/tiller.key.pem \
-#    --tiller-tls-verify \
-#    --tls-ca-cert ../keys/ca.cert.pem \
-#    --service-account tiller
 helm init --service-account tiller
 helm repo update
-
-echo "move keys"
-# cp ../keys/ca.cert.pem $(helm home)/ca.pem
-# cp ../keys/helm.cert.pem $(helm home)/cert.pem
-# cp ../keys/helm.key.pem $(helm home)/key.pem
-# helm ls --tls
 
 echo "verify helm"
 kubectl get deploy,svc tiller-deploy -n kube-system
---
+++ helm/scripts/add_secure_helm.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+echo "install helm"
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+kubectl --namespace kube-system create sa tiller
+kubectl create clusterrolebinding tiller \
+    --clusterrole cluster-admin \
+    --serviceaccount=kube-system:tiller
+
+echo "preclean"
+rm ca.* tiller.* helm.*
+
+echo "create certs"
+openssl genrsa -out ./ca.key.pem 4096
+openssl req -key ca.key.pem -batch -new -x509 -days 7300 -sha256 -out ca.cert.pem -extensions v3_ca
+openssl genrsa -out ./tiller.key.pem 4096
+openssl genrsa -out ./helm.key.pem 4096
+openssl req -key tiller.key.pem -batch -new -sha256 -out tiller.csr.pem
+openssl req -key tiller.key.pem -batch -new -sha256 -out helm.csr.pem
+openssl x509 -req -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -in tiller.csr.pem -out tiller.cert.pem
+openssl x509 -req -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -in tiller.csr.pem -out helm.cert.pem
+
+echo "initialize helm"
+kubectl create -f values/rbac-config.yaml
+helm init \
+    --tiller-tls \
+    --tiller-tls-cert ./tiller.cert.pem \
+    --tiller-tls-key ./tiller.key.pem \
+    --tiller-tls-verify \
+    --tls-ca-cert ./ca.cert.pem \
+    --service-account tiller
+helm repo update
+
+echo "verify helm"
+kubectl get deploy,svc tiller-deploy -n kube-system
