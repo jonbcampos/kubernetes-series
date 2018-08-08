10315dafc56dd0014054d4de5a99f5d5ddf80a4e
Jonathan Campos <jonbcampos@gmail.com>
2018-08-07 20:43:58 -0500

    fix paths

--- helm/scripts/add_secure_helm.sh
+++ helm/scripts/add_secure_helm.sh
@@ -9,8 +9,8 @@ kubectl create -f values/rbac-config.yaml
 echo "create tiller namespace"
 kubectl create namespace tiller
 kubectl create serviceaccount tiller --namespace tiller
-kubectl create -f role-tiller.yaml
-kubectl create -f rolebinding-tiller.yaml
+kubectl create -f values/role-tiller.yaml
+kubectl create -f values/rolebinding-tiller.yaml
 
 echo "preclean"
 rm ca.* tiller.* helm.*
