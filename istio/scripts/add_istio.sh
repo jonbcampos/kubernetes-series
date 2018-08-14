fa4c1f30c34cf4048d2d913c3953af3bdfa346ec
Jonathan Campos <jonbcampos@gmail.com>
2018-08-13 20:37:10 -0500

    istio

--- istio/scripts/add_istio.sh
+++ istio/scripts/add_istio.sh
@@ -6,6 +6,9 @@ echo "download istio"
 curl -L https://git.io/getLatestIstio | sh -
 cd istio-${ISTIO_VERSION}
 
+echo "add istio to the path"
+export PATH=$PWD/bin:$PATH
+
 echo "install istio"
 helm install install/kubernetes/helm/istio \
     --name istio \
