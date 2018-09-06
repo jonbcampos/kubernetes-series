d10b88fe98b29205d847b6bb7df7a561c85bdb95
Jonathan Campos <jonbcampos@gmail.com>
2018-09-06 14:52:34 -0500

    name fix

--- secrets/scripts/deploy.sh
+++ secrets/scripts/deploy.sh
@@ -11,6 +11,9 @@ export CONTAINER_NAME=${PROJECT_NAME}-container
 echo "setup"
 gcloud config set compute/zone ${INSTANCE_ZONE}
 
+echo "name replace"
+sed -i "s/PROJECT_NAME/${GCLOUD_PROJECT}/g" ../k8s/deployment.yaml
+
 echo "create pod-replicaset"
 kubectl apply -f ../k8s/basic-config.yaml
 kubectl apply -f ../k8s/deployment.yaml
