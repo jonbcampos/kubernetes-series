fe1e0045b60f6366e2d95f5092805785e31efa44
Jonathan Campos <jonbcampos@gmail.com>
2018-09-13 13:37:31 -0500

    adding deployment manager scripts

---
+++ deployment-manager/scripts/create-dm.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "preparing..."
+export GCLOUD_PROJECT=$(gcloud config get-value project)
+export INSTANCE_REGION=us-central1
+export INSTANCE_ZONE=us-central1-a
+export PROJECT_NAME=helm
+export CLUSTER_NAME=${PROJECT_NAME}-cluster
+export CONTAINER_NAME=${PROJECT_NAME}-container
+
+echo "setup"
+gcloud config set compute/zone ${INSTANCE_ZONE}
+
+echo "deploy with deployment manager"
+gcloud deployment-manager deployments create dm-cluster --config ../dm/cluster.yaml
